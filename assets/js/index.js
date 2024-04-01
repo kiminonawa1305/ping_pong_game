5cb56ed7849d3271783feb53e942fba0a36a4415
Lam Nguyen <kiminonawa1305@gmail.com>
2024-04-01 14:47:17 +0700

    Hoàn thanh cơ bản logic game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
-const screenWidth = (window.innerWidth - 18);
+/*
+* Name: Nguyễn Đình Lam
+* ID student: 21130416
+* Class: DH21DTC
+* Phone number: 0855354919
+*/
+
+const screenWidth = /*(window.innerWidth - 18)*/ 500;
 const screenHeight = window.innerHeight - 18;
 
 const soundEffectCollide = $('#sound_effect_collide')[0];
@@ -15,66 +22,26 @@ const barWidth = 200, barHeight = 10, spaseBarCompareToScreen = 50,
     barX = screenWidth / 2 - barWidth / 2,
     barY = screenHeight - barHeight - spaseBarCompareToScreen, ballSize = 10;
 
-const rndPoint = randomAngle(speed, speed, speed);
-
-let ballX = barX + barWidth / 2, ballY = barY - ballSize, dx = Math.random() > 0.5 ? -rndPoint[0] : rndPoint[0],
-    dy = rndPoint[1], run = false;
+let ballX = barX + barWidth / 2, ballY = barY - ballSize;
 
 let isMoveBar = false;
 let start = true;
-let level = 5;
-
+let level = 1;
+let run;
+let listBall;
+let listObstacle;
 const colors = ["#e11919", "#0b4689", "#000", "#036a0c", "#ab02b5", "#5b014a"];
-
+let bar;
 $(document).ready(() => {
-    const ball = new Circle(ballX, barY - ballSize, ballSize),
-        bar = new Rectangle(barX, barY, barWidth, barHeight);
-
-    const listObs = [bar];
-
-    ball.draw(contentCanvas)
-    bar.draw(contentCanvas);
-    const draw = () => {
-        if (!run) return;
-
-        let huong = TouchDirection.NONE;
-        /*Xử lý va chạm tường*/
-        while ((huong = checkCollide(ball, listObs, dx, dy, level)) !== TouchDirection.NONE) {
-            if (level === 4) {
-                ball.setColor(colors[Math.floor(Math.random() * colors.length)]);
-            }
-
-            if (huong === TouchDirection.WEST || huong === TouchDirection.EAST) {
-                getCollisionSound();
-                const rdPoint = randomAngle(dx, dy, speed);
-                dx = rdPoint[0]
-                // dy = Math.random() > 0.5 ? -rdPoint[1] : rdPoint[1]
-                dy = rdPoint[1]
-            }
-
-            if (huong === TouchDirection.SOUTH || huong === TouchDirection.NORTH) {
-                getCollisionSound();
-                const rdPoint = randomAngle(dy, dx, speed);
-                dy = rdPoint[0]
-                // dx = Math.random() > 0.5 ? -rdPoint[1] : rdPoint[1]
-                dx = rdPoint[1]
-            }
-        }
-
-        ballX += dx;
-        ballY += dy
-
-        ball.drawXY(ballX, ballY, contentCanvas);
-        bar.draw(contentCanvas);
-    }
+    newGame();
 
     /*event move bar and start game*/
     canvas.mousedown(event => {
         isMoveBar = true;
         if (start) {
-            start = false;
             run = true;
-            draw();
+            start = false;
+            playGame();
         }
     });
 
@@ -88,12 +55,97 @@ $(document).ready(() => {
         }
     });
 
-    setInterval(draw, 20)
-    mapLevel(level, listObs, contentCanvas, bar);
+
 });
 
+const newGame = () => {
+    contentCanvas.clearRect(0, 0, screenWidth, screenHeight);
+
+    listBall = [];
+    listObstacle = []
+
+    const barWidth = 200, barHeight = 10, spaseBarCompareToScreen = 50,
+        barX = screenWidth / 2 - barWidth / 2,
+        barY = screenHeight - barHeight - spaseBarCompareToScreen, ballSize = 10;
+    bar = new Rectangle(barX, barY, barWidth, barHeight);
+    for (let i = 0; i < 1; i++) {
+        const ball = new Circle(ballX, barY - ballSize, ballSize);
+        const rndPoint = randomAngle(speed, speed, speed);
+        const dx = Math.random() > 0.5 ? -rndPoint[0] : rndPoint[0],
+            dy = -rndPoint[1];
+        ball.setDxy(dx, dy);
+        ball.draw(contentCanvas);
+        listBall.push(ball);
+    }
+
+    listObstacle.push(bar);
+    bar.draw(contentCanvas);
+
+    drawObstacleByLevel(level, listObstacle, contentCanvas);
+}
+
+const playGame = () => {
+    let newListBall = listBall;
+    for (const index in listBall) {
+        if (listObstacle.length === 1) {
+            if (confirm(`Chúc mừng bạn đã chiến thắng level ${level}\nNhấn "OK" để tiếp tục chơi level ${++level}`)) {
+                newGame();
+                start = true;
+            }
+            return;
+        }
+
+        const ball = listBall.at(index);
+        let huong = TouchDirection.NONE;
+        let dx = ball.dx;
+        let dy = ball.dy;
+        /*Xử lý va chạm tường*/
+        while ((huong = checkCollide(listBall, ball, listObstacle, screenWidth, screenHeight, level)) !== TouchDirection.NONE) {
+            if (level === 4)
+                ball.setColor(colors[Math.floor(Math.random() * colors.length)]);
+
+            if (huong === TouchDirection.WEST || huong === TouchDirection.EAST) {
+                // getCollisionSound();
+                const rdPoint = randomAngle(dx, dy, speed);
+                dx = rdPoint[0]
+                // dy = Math.random() > 0.5 ? -rdPoint[1] : rdPoint[1]
+                dy = rdPoint[1]
+                ball.setDxy(dx, dy);
+            }
+
+            if (huong === TouchDirection.SOUTH || huong === TouchDirection.NORTH) {
+                // getCollisionSound();
+                const rdPoint = randomAngle(dy, dx, speed);
+                dy = rdPoint[0]
+                // dx = Math.random() > 0.5 ? -rdPoint[1] : rdPoint[1]
+                dx = rdPoint[1]
+                ball.setDxy(dx, dy);
+            }
+        }
+
+        if (ball.y - ball.radius > screenHeight) removeElement(newListBall, index)
+        // ball.drawXY(ball.x + dx, ball.y + dy, contentCanvas);
+    }
+
+    drawBall(listBall, contentCanvas);
+
+    listBall = newListBall;
+    bar.draw(contentCanvas);
+
+
+    if (listBall.length !== 0)
+        requestAnimationFrame(playGame);
+    else if (confirm("Thua mât rồi!\nBạn có muốn chơi lại không?")) {
+        level = 1;
+        newGame();
+        start = true;
+    }
+}
+
+
 /*Chạm hướng từ dưới lên*/
-function collideNorth(ball, collidePoint, dy) {
+function collideNorth(ball, collidePoint) {
+    const dy = ball.dy;
     if (dy > 0) return false;
 
     const distance = distanceCollideNorth(ball, collidePoint);
@@ -101,7 +153,8 @@ function collideNorth(ball, collidePoint, dy) {
 }
 
 /*chạm hướng từ trên xuống*/
-function collideSouth(ball, collidePoint, dy) {
+function collideSouth(ball, collidePoint) {
+    const dy = ball.dy;
     if (dy < 0) return false;
 
     const distance = distanceCollideSouth(ball, collidePoint);
@@ -109,53 +162,60 @@ function collideSouth(ball, collidePoint, dy) {
 }
 
 /*Chạm hướng từ trái qua phải*/
-function collideEast(ball, collidePoint, dx) {
+function collideEast(ball, collidePoint) {
+    const dx = ball.dx;
     if (dx < 0) return false;
     const distance = distanceCollideEast(ball, collidePoint);
     return 0 <= distance && distance < dx
 }
 
+/*Chạm hướng từ phải qua trái*/
+function collideWest(ball, collidePoint) {
+    const dx = ball.dx;
+    if (dx > 0) return false;
+    const distance = distanceCollideWest(ball, collidePoint);
+    return 0 <= distance && distance < Math.abs(dx)
+}
+
 /*Kiểm tra xem quả banh có chạm vào cc phần tử khác có trong map không*/
-function checkCollide(ball, list, dx, dy, level) {
-    const isCollideWall = collideWall(ball, dx, dy);
+function checkCollide(listBall, ball, listObstacle, screenWidth, screenHeight, level) {
+    const dx = ball.dx;
+    const dy = ball.dy;
+    const isCollideWall = collideWall(ball, screenWidth, screenHeight);
+    if (isCollideWall === TouchDirection.SOUTH) return TouchDirection.NONE
     if (isCollideWall !== TouchDirection.NONE) return isCollideWall;
     let result = TouchDirection.NONE;
-    for (let index = 0; index < list.length; index++) {
-        result = list[index].collide(ball, dx, dy);
+    for (let index = 0; index < listObstacle.length; index++) {
+        result = listObstacle[index].collide(ball, dx, dy);
 
         if (result === TouchDirection.NONE)
             continue;
 
-        if (level >= 4 && list[index].color !== ball.color) {
+        if (level >= 4 && listObstacle[index].color !== ball.color) {
             ball.setColor(colors[Math.floor(Math.random() * colors.length)]);
             return result;
         }
 
-        if (list[index] instanceof Obstacle) {
-            if (isNaN(list[index].blood) || list[index].blood === 0) {
-                contentCanvas.clearRect(list[index].x, list[index].y, list[index].width, list[index].height);
-                list[index].setPoint(-100000, -100000);
-                list[index].width = 0;
-                list[index].height = 0;
-                list[index].draw(contentCanvas);
+        if (listObstacle[index] instanceof Obstacle) {
+            if (isNaN(listObstacle[index].blood) || listObstacle[index].blood === 0) {
+                contentCanvas.clearRect(listObstacle[index].x, listObstacle[index].y, listObstacle[index].width, listObstacle[index].height);
+                listObstacle[index].setPoint(-100000, -100000);
+                listObstacle[index].width = 0;
+                listObstacle[index].height = 0;
+                listObstacle[index].draw(contentCanvas);
             }
 
-            if (list[index].blood > 0) {
-                list[index].drawBlood(contentCanvas);
+            if (listObstacle[index].blood > 0) {
+                listObstacle[index].drawBlood(contentCanvas);
             }
+            listObstacle = removeElement(listObstacle, index);
         }
+
 
         return result;
     }
 
     return TouchDirection.NONE
-}
-
-/*Chạm hướng từ phải qua trái*/
-function collideWest(ball, collidePoint, dx) {
-    if (dx > 0) return false;
-    const distance = distanceCollideWest(ball, collidePoint);
-    return 0 >= distance && distance > dx
 }
 
 /*Tính khoảng cách từ chái banh đến điểm chạm từ dưới lên trêni*/
@@ -179,7 +239,8 @@ function distanceCollideEast(ball, collidePoint) {
 }
 
 /*Kiểm tra banh có chạm viên gạch theo hướng trên xuống dưới*/
-function collideSouthObstacle(ball, bar, dy) {
+function collideSouthObstacle(ball, bar) {
+    const dy = ball.dy;
     const collidePoint = bar.y;
     if ((ball.x + ball.radius) >= (bar.x - ball.radius) && (ball.x - ball.radius) <= (bar.x + bar.width + ball.radius))
         return collideSouth(ball, collidePoint, dy);
@@ -187,7 +248,8 @@ function collideSouthObstacle(ball, bar, dy) {
 }
 
 /*Kiểm tra banh có chạm viên gạch theo hướng dưới lên trên*/
-function collideNorthObstacle(ball, bar, dy) {
+function collideNorthObstacle(ball, bar) {
+    const dy = ball.dy;
     const collidePoint = bar.y + bar.height;
     if ((ball.x + ball.radius) >= (bar.x - ball.radius) && (ball.x - ball.radius) <= (bar.x + bar.width + ball.radius))
         return collideNorth(ball, collidePoint, dy);
@@ -195,7 +257,8 @@ function collideNorthObstacle(ball, bar, dy) {
 }
 
 /*Kiểm tra banh có chạm viên gạch theo hướng phải sang trái*/
-function collideWestObstacle(ball, bar, dx) {
+function collideWestObstacle(ball, bar) {
+    const dx = ball.dx;
     const collidePoint = bar.x + bar.width;
 
     if ((ball.y + ball.radius) >= (bar.y - ball.radius) && (ball.y - ball.radius) <= (bar.y + bar.height + ball.radius))
@@ -204,7 +267,8 @@ function collideWestObstacle(ball, bar, dx) {
 }
 
 /*Kiểm tra banh có chạm viên gạch theo hướng trái sang phải*/
-function collideEastObstacle(ball, bar, dx) {
+function collideEastObstacle(ball, bar) {
+    const dx = ball.dx;
     const collidePoint = bar.x;
 
     if ((ball.y + ball.radius) >= (bar.y - ball.radius) && (ball.y - ball.radius) <= (bar.y + bar.height + ball.radius))
@@ -230,7 +294,6 @@ function randomAngle(inverseNumber, affect, speed) {
 /*Sự kiện duy chuyển thanh bar*/
 const moveBar = (bar, clientX, screenWidth, contentCanvas) => {
     const barWidth = bar.width;
-    const barHeight = bar.height;
     const barY = bar.y;
     let barX = bar.x;
 
@@ -241,12 +304,26 @@ const moveBar = (bar, clientX, screenWidth, contentCanvas) => {
     bar.drawXY(barX, barY, contentCanvas);
 }
 
+function removeElement(list, index) {
+    return list.splice(index, 1)
+}
+
+function drawBall(listBall, contentCanvas) {
+    listBall.map(ball => {
+        ball.clearDraw(contentCanvas)
+    })
+
+    listBall.map(ball => {
+        ball.drawXY(ball.x + ball.dx, ball.y + ball.dy, contentCanvas);
+    })
+}
+
 /*Kểm tra chạm tường*/
-function collideWall(ball, dx, dy) {
-    if (collideEast(ball, screenWidth, dx)) return TouchDirection.EAST
-    if (collideWest(ball, 0, dx)) return TouchDirection.WEST
-    if (collideNorth(ball, 0, dy)) return TouchDirection.NORTH
-    if (collideSouth(ball, screenHeight, dy)) return TouchDirection.SOUTH
+function collideWall(ball, screenWidth, screenHeight) {
+    if (collideEast(ball, screenWidth)) return TouchDirection.EAST
+    if (collideWest(ball, 0)) return TouchDirection.WEST
+    if (collideSouth(ball, screenHeight)) return TouchDirection.SOUTH
+    if (collideNorth(ball, 0)) return TouchDirection.NORTH
     return TouchDirection.NONE;
 }
 
@@ -293,13 +370,14 @@ class Circle {
         contentCanvas.fill();
     }
 
-    drawXY = (x, y, contentCanvas) => {
-        contentCanvas.clearRect(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
+    drawXY(x, y, contentCanvas) {
+        // this.clearDraw(contentCanvas);
         this.setPoint(x, y);
-        contentCanvas.beginPath();
-        contentCanvas.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        contentCanvas.fillStyle = this.color
-        contentCanvas.fill();
+        this.draw(contentCanvas);
+    }
+
+    clearDraw(contentCanvas) {
+        contentCanvas.clearRect(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
     }
 
     setColor(color) {
@@ -310,6 +388,12 @@ class Circle {
         this.x = x;
         this.y = y;
     }
+
+    setDxy(dx, dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
 }
 
 /*Khởi tạo class hình chử nhật*/
@@ -333,10 +417,7 @@ class Rectangle {
     drawXY(x, y, contentCanvas) {
         contentCanvas.clearRect(this.x, this.y, this.width, this.height);
         this.setPoint(x, y);
-        contentCanvas.beginPath();
-        contentCanvas.rect(this.x, this.y, this.width, this.height);
-        contentCanvas.fillStyle = this.color
-        contentCanvas.fill();
+        this.draw(contentCanvas);
     }
 
     setPoint(x, y) {
@@ -349,10 +430,10 @@ class Rectangle {
     }
 
     collide(ball, dx, dy) {
-        if (collideSouthObstacle(ball, this, dy)) return TouchDirection.SOUTH
-        if (collideNorthObstacle(ball, this, dy)) return TouchDirection.NORTH
-        if (collideEastObstacle(ball, this, dx)) return TouchDirection.EAST
-        if (collideWestObstacle(ball, this, dx)) return TouchDirection.WEST
+        if (collideSouthObstacle(ball, this)) return TouchDirection.SOUTH
+        if (collideNorthObstacle(ball, this)) return TouchDirection.NORTH
+        if (collideEastObstacle(ball, this)) return TouchDirection.EAST
+        if (collideWestObstacle(ball, this)) return TouchDirection.WEST
         return TouchDirection.NONE;
     }
 }
@@ -412,11 +493,11 @@ function getCollisionSound() {
 }
 
 /*Tạo kết quả khi banh đụng tường*/
-const TouchDirection = {"EAST": 1, "WEST": 2, "SOUTH": 3, "NORTH": 4, "NONE": 0}
+const TouchDirection = {"EAST": 1, "WEST": 2, "SOUTH": 3, "NORTH": 4, "NONE": 0, "REMOVE": -1}
 
 /*Tạo sự kiện theo level*/
 const level1 = (listObstacle, contentCanvas) => {
-    const rows = 10;
+    const rows = 1;
     createArrayObstacleWithRows(listObstacle, rows);
     drawObstacle(listObstacle, contentCanvas);
 }
@@ -477,7 +558,7 @@ const dropObstacle = (listObstacle, contentCanvas) => {
         }
     }, 10000);
 }
-const mapLevel = (level, listObs, contentCanvas) => {
+const drawObstacleByLevel = (level, listObs, contentCanvas) => {
     switch (level) {
         case 1: {
             level1(listObs, contentCanvas)
